f1c2335dfeec6d645314518bd7d03a1ca199e871
Géza Kenyeres <kenyeres.geza@webtown.hu>
2023-12-18 20:18:35 +0100

    Add Docker publish

--- docker-run-from-local.ps1
+++ docker-run-from-local.ps1
@@ -1,2 +1,2 @@
 docker build -f Dockerfile-aftermaven -t cloud-native-demo:tutorial .
-docker run -p 8080:8080 --rm --name demo -it cloud-native-demo:tutorial
+docker run -p 8090:8080 --rm --name demo -it cloud-native-demo:tutorial
